986b8506651affb6107ef6f2e50b04d1fdcbb5b7
Daniel <daniel.petlaraujo@gmail.com>
2026-01-23 10:31:20 +0100

    Creating the week12 folder

---
+++ Week12/is_prime.py
@@ -0,0 +1,8 @@
+def is_prime(n):
+    # TODO Implement this function
+
+num = int(input("Please enter a number: "))
+if is_prime(num):
+    print(f"{num} is a prime")
+else:
+    print(f"{num} is not a prime")
