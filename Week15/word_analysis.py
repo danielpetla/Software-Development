d9e1ee4685d6c6116a3bf2076e8450c255281000
Daniel <daniel.petlaraujo@gmail.com>
2026-02-14 18:43:06 +0100

    New exercises solved in the document word_analysis.py

--- Week15/word_analysis.py
+++ Week15/word_analysis.py
@@ -8,18 +8,44 @@ file_name = "text.txt"
 with pathlib.Path(file_name).open("rt", encoding="utf-8") as f:
     text = f.readlines()
 
-
 # Find all words that appear
 words = set(w.lower() for line in text for w in line.split())
 
-# Count how often each appears
+# ---------
+
+# Count how often each word appears
 counts = {}
 for line in text:
     for w in line.split():
         word = w.lower()
-        # go to the key (word) and increment the value tha is related to it
+        # go to the key (word) and increment the value that is related to it
         counts[word] = counts.get(word, 0) + 1
 # Print out the counts
 for key, value in sorted(counts.items(), key=lambda item: item[1], reverse=True):
     if value > 4:
         print(f"{value}: {key}")
+
+# ---------
+# Finds all the characters in the book
+letters = (le.lower() for line in text for w in line.split() for le in list(w))
+
+# Counting how often each char appears
+counts_l = {}
+for line in text:
+    for w in line.split():
+        for le in list(w.lower()):
+            letter = le.lower()
+            # go to the key (letter) and increment the value that is related to it
+            counts_l[letter] = counts_l.get(letter, 0) + 1
+
+total = 0
+for c, v in counts_l.items():
+    total += v
+
+for c, v in counts_l.items():
+    avg = (v/total) * 100
+    counts_l[c] = avg
+
+for key, value in sorted(counts_l.items(), key=lambda item: item[1], reverse=True):
+    if value >= 0.01:
+        print(f"{value:.2f}%: {key}")
