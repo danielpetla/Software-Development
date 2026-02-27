42f10cb47bf00575081c431fc3b9bff6408d664d
Daniel <daniel.petlaraujo@proton.me>
2026-02-27 11:53:26 +0100

    First changes

--- Week17/read_csv.py
+++ Week17/read_csv.py
@@ -1,7 +1,7 @@
 import csv
 from pathlib import Path
 
-with Path("<put correct name here>").open("<put correct code here>") as f:
+with Path("/Users/daniel.petla/Documents/GitHub/Software-Development/Week17/grades.csv").open("rt") as f:
     rows = f.readlines() # TODO Replace with csv reader
 
 for row in rows:
