2ce03616dd85b9b448390426deca2111dfd705ba
Daniel <daniel.petlaraujo@proton.me>
2026-03-03 20:59:36 +0100

    Eggs first part of code

--- Coursework/eggmorph.py
+++ Coursework/eggmorph.py
@@ -1,5 +1,11 @@
-first_input = input()
-numbers_str = first_input.split()
-numbers = [int(num) for num in numbers_str]
+# getting the number of n, n and k
+spec_input = input()
+numbers_str = spec_input.split()
+numbers = [int(num) for num in numbers_str]  # splitting the numbers of the input
 
-mnk = {'m': numbers[0], 'n': numbers[1], k: numbers[2]}
+mnk = {'n': numbers[0], 'm': numbers[1], k: numbers[2]}  # connecting the values to the right keys
+
+# getting the input os the morphs
+nest_input = input()
+unfold = nest_input.split()
+fi_ni = {'fi': unfold[0], 'ni': unfold[1]}
