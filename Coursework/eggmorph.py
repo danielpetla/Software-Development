3e02f6d11be12125ea995148f87d52e5bef9822c
Daniel <daniel.petlaraujo@proton.me>
2026-03-03 21:26:13 +0100

    Eggs core idea completed

--- Coursework/eggmorph.py
+++ Coursework/eggmorph.py
@@ -3,9 +3,23 @@ spec_input = input()
 numbers_str = spec_input.split()
 numbers = [int(num) for num in numbers_str]  # splitting the numbers of the input
 
-mnk = {'n': numbers[0], 'm': numbers[1], k: numbers[2]}  # connecting the values to the right keys
+mnk = {'n': numbers[0], 'm': numbers[1], 'k': numbers[2]}  # connecting the values to the right keys
+
+state_input = input()
+spl_str = state_input.split()
+state = [int(s) for s in spl_str]  # splitting the numbers of the input
+
 
 # getting the input os the morphs
 nest_input = input()
 unfold = nest_input.split()
-fi_ni = {'fi': unfold[0], 'ni': unfold[1]}
+w_input = [int(m) for m in unfold]
+feeding_nests = w_input[2:2+fi]
+nursing_nests = w_input[2+fi:2+fi+ni]
+
+# subtract 1 egg from them
+for nest in feeding_nests:
+    state[nest-1] -= 1  # -1 because nests are 1-indexed in input
+
+for nest in nursing_nests:
+    state[nest-1] += 1  # -1 because nests are 1-indexed in input
