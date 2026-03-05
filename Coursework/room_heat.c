f9fbd6f39e72c221cd5cc72300115edbd276d074
Daniel <daniel.petlaraujo@proton.me>
2026-03-05 10:51:20 +0100

    starting the room heat code

--- Coursework/room_heat.c
+++ Coursework/room_heat.c
@@ -1,6 +1,27 @@
-# include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 
 int main(){
+    int n, m, k;
 
+    if (scanf("%d %d %d", &n, &m, &k) != 3)
+        return 1;
+
+    int **pipes = malloc(m * sizeof(int *));
+    int *pipe_sizes = malloc(m * sizeof(int));
+
+
+    for (int i = 0; i < m; i++) {
+        int num_rooms;
+        scanf("%d", &num_rooms);
+
+        pipe_sizes[i] = num_rooms;
+        pipes[i] = malloc(num_rooms * sizeof(int));
+
+        for (int j = 0; j < num_rooms; j++) {
+            scanf("%d", &pipes[i][j]);
+
+        }
+
+    }
 }
