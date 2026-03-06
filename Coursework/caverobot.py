a166338ccd927718d5bcdf169e390eb663121d0b
Daniel <daniel.petlaraujo@proton.me>
2026-03-06 20:55:14 +0100

    caverobot prototype 2

--- Coursework/caverobot.py
+++ Coursework/caverobot.py
@@ -2,96 +2,93 @@ from collections import deque
 import sys
 import string
 
-def solve_maze():
-    lines = [line.strip() for line in sys.stdin.read().splitlines() if line.strip()]
-    if not lines: return -1
+def solve():
+    data = sys.stdin.read().split()
+    if not data: return
 
-    if lines[0][0].isdigit():
-            grid = lines[1:]
-    else:
-        grid = lines
+    w = int(data[0])  # rows
+    h = int(data[1])  # collumns
+    grid = data[2:2 + h]
 
-    rows, cols = len(grid), len(grid[0])  # w and h
-
-    start = end = None
     total_plates = {}
+    wormholes = {str(i): [] for i in range(1, 10)}
+    start = None
 
-    # Initial scan
-    for r in range(rows):
-        for c in range(cols):
+    # pre-scan
+    for r in range(h):
+        for c in range(w):
             char = grid[r][c]
-            if char == 'S': start = (r, c)  # location of the start
-            if char == 'E': end = (r, c)  # location of the end
-            elif char.islower():  # = pressure plate
+            if 'a' <= char <= 'z':
                 total_plates[char] = total_plates.get(char, 0) + 1
+            elif '1' <= char <= '9':
+                wormholes[char].append((r, c))
+            elif char == '=':
+                start = (r, c)
 
-    # safety check:
-    if start is None or end is None:
-        return - 1 # exit is unrechable
+    if not start:
+        print("No")
+        return
 
-    # state track
-    plates_found = {k: 0 for k in total_plates}
-    door_open = {}
-    unsure_doors = {}
+    # state initialize
+    initial_pressed = frozenset()
+    visited = set([(start[0], start[1], initial_pressed)])
 
-    # preconfigure doors
-    for k in string.ascii_uppercase:
-        door_open[k] = (k.lower() not in total_plates)
-        unsure_doors[k] = []
+    # tracks: row, col, pressed plates, distance
+    q = deque([(start[0], start[1], initial_pressed, 0)])
 
-    # inialize search
-    visited = set([start])
-    queue = deque([(start[0], start[1], 0)])  # stores: row, col, distance
+    dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-    # The search
-    while queue:
-        r, c, dist = queue.popleft()
+    # Search
+    while q:
+        r, c, pressed, dist = q.popleft()
+        char = grid[r][c]
 
-        if (r,c) == end:
-            return dist  # exit found
+        # exit found
+        if char == '!':
+            print("Yes")
+            return
 
-        # orthogonal exploring
-        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0,1)]:
+        # wormhole
+        if '1' <= char <= '9':
+            for wr, wc in wormholes[char]:
+                # if wormhole not visited -> exit
+                if (wr, wc, pressed) not in visited:
+                    visited.add((wr, wc, pressed))
+                    # moving
+                    q.append((wr, wc, pressed, dist))
+
+        # orthogonal moviment
+        for dr, dc in dirs:
             nr, nc = r + dr, c + dc
 
-            # bounderys and visited
-            if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in visited:
-                char = grid[nr][nc]
+            if 0 <= nr < h and 0 <= nc < w:
+                nchar = grid[nr][nc]
 
-                if char == "#":  # wall
+                if nchar == '#':
                     continue
 
-                if char.isupper() and char not in ['S', 'E']:  # it is a door
-                    if not door_open[char]:
-                        # door is locked: save position and follow other path
-                        unsure_doors[char].append((nr, nc, dist + 1))
-                        continue
+                # doors
+                if 'A' <= nchar <= 'Z':
+                    req_plate = nchar.lower()
+                    if req_plate in total_plates:
+                        # count plates pressed
+                        pressed_count = sum(1 for pr, pc in pressed if grid[pr][pc] == req_plate)
+                        if pressed_count < total_plates[req_plate]:
+                            continue  # not possible
 
-                # cell is walkable
-                visited.add((nr, nc))
-                queue.append((nr, nc, dist + 1))
+                # plates
+                new_pressed = pressed
+                if 'a' <= nchar <= 'z' and (nr, nc) not in pressed:
+                    new_pressed = pressed | frozenset([(nr, nc)])
 
-                if char.islower():  # plate
-                    plates_found[char] += 1
+                # check visited
+                if (nr, nc, new_pressed) not in visited:
+                    visited.add((nr, nc, new_pressed))
+                    q.append((nr, nc, new_pressed, dist + 1))
 
-                    # find if it is the last plate of the type
-                    if plates_found[char] == total_plates[char]:
-                        door_char = char.upper()
-                        door_open[door_char] = True  # unlock door
+    # not possible
+    print("No")
 
-                        # all doors in waiting
-                        for pr, pc, pdist in unsure_doors[door_char]:
-                            if (pr, pc) not in visited:
-                                visited.add((pr, pc))
-                                queue.append((pr, pc, pdist))
-
-                        # clear pending of this door type
-                        unsure_doors[door_char] = []
-
-    # exit not reachable
-    return -1
-
-
-# Trigger the function
+# Trigger function
 if __name__ == '__main__':
-    print(solve_maze())
+    solve()
