6f5d91ae6bebc1f701f9163929c53e880f20add6
Daniel <daniel.petlaraujo@proton.me>
2026-03-14 17:30:57 +0100

    Week 19 exercise 3

--- Week19/grades.py
+++ Week19/grades.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 import csv
+import logging
 
 print("Opening file for reading")
 with Path("grades.csv").open("rt") as f:
-	students = list(csv.DictReader(f))
+        students = list(csv.DictReader(f))
 print(f"Read {len(students)} student from file")
 
 print("Computing averages")
@@ -15,23 +16,23 @@ print(f"Better than 60%: {', '.join(s['Firstname'] + ' ' + s['Lastname'] for s i
 print()
 
 def is_better_b(grade):
-	return grade in {"A+", "A", "A-", "B+", "B", "B-"}
+        return grade in {"A+", "A", "A-", "B+", "B", "B-"}
 
 tests = [f"Test{i}" for i in range(1, 5)]
 
 print("B- or better:")
 for s in students:
-	if not is_better_b(s["Grade"]):
-        print(f"{s} worse than B-")
-		continue
-	print(f"  {s['Firstname']} {s['Lastname']}: {' '.join(s[t] for t in tests)} {s['Final']}")
+        if not is_better_b(s["Grade"]):
+                print(f"{s} worse than B-")
+                continue
+        print(f"  {s['Firstname']} {s['Lastname']}: {' '.join(s[t] for t in tests)} {s['Final']}")
 
 print("Opening output file for writing")
 with Path("out.csv").open("wt") as f:
-	writer = csv.writer(f)
-	writer.writerow(["Name", "Average", "Best"])
-	for s in students:
-		average = sum(float(s[t]) for t in tests) / len(tests)
-		best = max(tests, key=s.__getitem__)
-		writer.writerow([f"{s['Firstname']} {s['Lastname']}", average, best])
+        writer = csv.writer(f)
+        writer.writerow(["Name", "Average", "Best"])
+        for s in students:
+                average = sum(float(s[t]) for t in tests) / len(tests)
+                best = max(tests, key=s.__getitem__)
+                writer.writerow([f"{s['Firstname']} {s['Lastname']}", average, best])
 print("Done writing")
