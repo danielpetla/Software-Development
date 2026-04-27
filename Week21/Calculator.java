770d90e2180a2d75f8a62838f97015e0d517d5f1
Daniel <daniel.petlaraujo@proton.me>
2026-04-27 11:15:27 +0200

    Week 21 first commmit

---
+++ Week21/Calculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter: ");
+        double first = scanner.nextDouble();
+        String operation = scanner.next();
+        double second = scanner.nextDouble();
+
+        // TODO Compute and print the result
+    }
+}
