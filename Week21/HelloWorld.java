21aef9a9079fc81eccaa6438fb1c9ef57c10909b
Daniel <daniel.petlaraujo@proton.me>
2026-04-27 14:06:22 +0200

    Exercise 2

--- Week21/HelloWorld.java
+++ Week21/HelloWorld.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Scanner scanner = new Scanner(System.in);
+
+        String str = "";
+        if (scanner.hasNextLine()){
+            str = scanner.nextLine();
+        }
+        System.out.printf("Hello, %s%n", str);  // %n = \n (in C)
     }
 }
